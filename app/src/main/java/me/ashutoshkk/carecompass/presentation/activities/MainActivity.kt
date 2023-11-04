9a0917f7749aaef61bebf72883c8f8f654749c09
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-11-05 01:25:50 +0530

    Setup Bottom Navigation Bar with fragments

--- app/src/main/java/me/ashutoshkk/carecompass/presentation/activities/MainActivity.kt
+++ app/src/main/java/me/ashutoshkk/carecompass/presentation/activities/MainActivity.kt
@@ -2,11 +2,29 @@ package me.ashutoshkk.carecompass.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import me.ashutoshkk.carecompass.R
+import me.ashutoshkk.carecompass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding : ActivityMainBinding? = null
+    private val binding : ActivityMainBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationBar.setupWithNavController(navController)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/me/ashutoshkk/carecompass/presentation/fragments/prescription/PrescriptionFragment.kt
+++ app/src/main/java/me/ashutoshkk/carecompass/presentation/fragments/prescription/PrescriptionFragment.kt
@@ -1,26 +1,27 @@
 package me.ashutoshkk.carecompass.presentation.fragments.prescription
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import me.ashutoshkk.carecompass.R
-import me.ashutoshkk.carecompass.databinding.FragmentPredictionBinding
+import me.ashutoshkk.carecompass.databinding.FragmentPrescriptionBinding
 import me.ashutoshkk.carecompass.presentation.viewModels.PrescriptionViewModel
 
 class PrescriptionFragment : Fragment() {
 
     private lateinit var viewModel: PrescriptionViewModel
-    private var _binding: FragmentPredictionBinding? = null
-    private val binding: FragmentPredictionBinding get() = _binding!!
+    private var _binding: FragmentPrescriptionBinding? = null
+    private val binding: FragmentPrescriptionBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentPredictionBinding.inflate(inflater, container, false)
+        _binding = FragmentPrescriptionBinding.inflate(inflater, container, false)
+
+
+
         return binding.root
     }
 
